3f3f4741f0050e0916838c96bfaa0bd25c3f5446
rocifani <rociobfani@hotmail.com>
2024-03-05 11:55:32 -0300

    cambio en migration informe final

--- tp/database/migrations/2024_03_05_102404_create_informe_final_table.php
+++ tp/database/migrations/2024_03_05_102404_create_informe_final_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('table_informe_final', function (Blueprint $table) {
+        Schema::create('informe_final', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->foreing('id_pps')->references('id_pps')->on('pps');
