a07715d344a4fe4688bdda3666400b11677bfcea
rocifani <rociobfani@hotmail.com>
2024-03-05 11:31:22 -0300

    agregado de migration rol

--- tp/database/migrations/2024_03_05_112751_create_rol_table.php
+++ tp/database/migrations/2024_03_05_112751_create_rol_table.php
@@ -11,22 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('rol', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->foreign('id_rol')->references('id_rol')->on('rol');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
-    #hola
+
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('rol');
     }
 };
