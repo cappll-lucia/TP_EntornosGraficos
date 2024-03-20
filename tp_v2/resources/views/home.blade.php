2c9c9365945d51c23dda438c74e79dc0179111bc
rocifani <rociobfani@hotmail.com>
2024-03-20 09:11:50 -0300

    home blade

---
+++ tp_v2/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends ('layouts.home')
+@section ('content')
+
+<div>
+  <h1>
+    ¡Bienvenido!
+  </h1>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
+
+@endsection
