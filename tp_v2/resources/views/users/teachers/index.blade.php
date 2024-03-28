7865eca210e78bd99f7845857e35b2b1d8f3c010
rocifani <rociobfani@hotmail.com>
2024-03-28 15:18:10 -0300

    ABM Students primera parte

--- tp_v2/resources/views/users/teachers/index.blade.php
+++ tp_v2/resources/views/users/teachers/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div class=" page font-sans antialiased dark:bg-black dark:text-white/50" style="--bs-bg-opacity:.5">
+    <div class=" font-sans page antialiased dark:bg-black dark:text-white/50" style="--bs-bg-opacity:.5">
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
             <div class="">
                 <div class="">
@@ -8,10 +8,10 @@
             <div class="container mt-3">
 
         <div class="bg-light mx-auto mb-3 card">
-          <div class="card-header d-flex justify-content-between flex-row align-items-center ">
-            <h3 class=" h3 mt-2 text-center card-title">Docente</h3>
+          <div class="flex-row card-header d-flex justify-content-between align-items-center ">
+            <h3 class=" mt-2 text-center card-title h3">Docente</h3>
             @if(Auth::check() && Auth::user()->role_id===4)
-            <button type="button submit" class="btn btn-success d-block right-0"> <a href="{{ route('createTeacher') }}">
+            <button type="button submit" class="right-0 btn btn-success d-block"> <a href="{{ route('createTeacher') }}">
             <i class="fa-solid fa-plus"></i></a></button>
             @endif
 
@@ -49,7 +49,7 @@
                                                 <div class="modal-dialog">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
-                                                            <h5 class="h5 modal-title" id="deleteTeacherModalLabel">Eliminar Docente</h5>
+                                                            <h5 class="modal-title h5" id="deleteTeacherModalLabel">Eliminar Docente</h5>
                                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                         </div>
                                                         <div class="modal-body">
@@ -81,7 +81,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="h1 modal-title fs-5" id="exampleModalLabel">Eliminar Docente</h1>
+        <h1 class="modal-title h1 fs-5" id="exampleModalLabel">Eliminar Docente</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
@@ -91,10 +91,10 @@
         </span>
       </div>
       <div class="modal-footer">
-                   <a type="button" class="btn btn-outline-secondary w-50 me-1" href="{{ route('getTeachers') }}">
+                   <a type="button" class="w-50 btn btn-outline-secondary me-1" href="{{ route('getTeachers') }}">
                 Cancelar
             </a>
-          <button type="submit ml-3" class="btn btn-outline-primary w-50 ms-1">
+          <button type="submit ml-3" class="w-50 btn btn-outline-primary ms-1">
             <a class="text-sm text-gray-600 underline" href="{{ route('storeNewTeacher') }}">
             </a>
               Guardar Cambios
