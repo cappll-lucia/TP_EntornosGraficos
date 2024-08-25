4daef1f948b277a54d59cff9c9c55e1df8d410e8
AgustinaRodriguez01 <agus4956@gmail.com>
2024-08-25 20:09:47 -0300

    finalreport and weeklytracking controllers created

---
+++ tp_v2/app/Http/Controllers/FinalReportController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\PPS;
+use App\Models\FinalReport;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
+
+class FinalReportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function download($id)
+    {
+        try {
+            $pps = PPS::findOrFail($id)->load('FinalReport');
+            $fr = $pps->FinalReport;
+            if (Storage::exists($fr->file_path)) {
+                return response()->download(storage_path('app/' . $fr->file_path));
+            }
+            return response()->json([
+                'success' => false,
+                'title' => 'Error al descargar el reporte',
+                'message' => 'El archivo no existe'
+            ], 400);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'title' => 'Error al descargar el reporte',
+                'message' => 'Intente nuevamente o comuníquese para soporte',
+                'error' => $e->getMessage()
+            ], 400);
+        }
+    }
+
+    public function upload(Request $request)
+    {
+        try {
+            $pps = PPS::find($request->input('pps_id'));
+            $student = User::where('user_id', auth()->user()->id)->first();
+            if ($pps->student_id != $student->id || auth()->user()->role_id != 2) {
+                return response()->json([
+                    'success' => false,
+                    'title' => 'Error al subir el reporte',
+                    'message' => 'No está autorizado a realizar esta acción'
+                ], 400);
+            }
+
+            DB::beginTransaction();
+            $file = $request->file('file');
+            if ($file->isValid()) {
+                $path = $file->store('public/final_reports');                
+                FinalReport::create([
+                    'pps_id' => $pps->id,
+                    'file_path' => $path,
+                    'is_accepted' => false,
+                    'observations' => ''
+                ]);
+
+                /*Mail::to($pps->Teacher->User->email)->send(
+                    new UploadFinalReportEmail(
+                        $pps->Student->lastname . ', ' . $pps->Student->first_name,
+                        $pps->Student->User->email,
+                        $pps->id,
+                        $pps->Teacher->name
+                    )
+                );*/
+                
+                $pps->is_finished = true;
+                $pps->save();
+
+                DB::commit();
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Reporte subido correctamente',
+                    'data' => $pps
+                ], 201);
+            }
+            DB::rollBack();
+            return response()->json([
+                'success' => false,
+                'title' => 'Error al subir el reporte',
+                'message' => 'El archivo no es válido'
+            ], 400);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'success' => false,
+                'title' => 'Error al subir el reporte',
+                'message' => 'Intente nuevamente o comuníquese para soporte',
+                'error' => $e->getMessage()
+            ], 400);
+        }
+    }
+}
---
+++ tp_v2/app/Http/Controllers/WeeklyTrackingController.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\PPS\PPSUpdateRequest;
+use App\Models\PPS;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use PhpParser\Node\Stmt\TryCatch;
+use App\Models\WeeklyTracking;
+
+class WeeklyTrackingController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function delete(string $id)
+    {
+        try {
+            $wt = WeeklyTracking::findOrFail($request->input('id'))->load('PPS');
+            $rol = auth()->user()->role_id;
+            if (($rol != 1 && $rol != 2) || ($rol != 1 && $wt->PPS->student_id != auth()->user()->User->id)) {
+                return response()->json([
+                    'success' => false,
+                    'title' => 'Error al eliminar la solicitud',
+                    'message' => 'No está autorizado a realizar esta acción'
+                ], 400);
+            }
+            if (Storage::exists($wt->file_path)) {
+                Storage::delete($wt->file_path);
+            }
+            $wt->delete();
+            return response()->json([
+                'success' => true,
+                'message' => 'Seguimiento eliminado correctamente'
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'title' => 'Error al eliminar la seguimiento',
+                'message' => 'Intente nuevamente o comuníquese para soporte',
+                'error' => $e->getMessage()
+            ], 400);
+        }
+    }
+
+    public function accept(Request $request)
+    {
+        try {
+            $wt = WeeklyTracking::findOrFail($request->input('id'))->load('PPS');
+            $pps = $wt->PPS;
+            $rol = auth()->user()->role_id;
+            if ($rol != 3 || $wt->PPS->teacher_id != auth()->user()->User->id) {
+                return response()->json([
+                    'success' => false,
+                    'title' => 'Error al aceptar el seguimiento',
+                    'message' => 'No está autorizado a realizar esta acción'
+                ], 400);
+            }
+            $wt->is_accepted = true;
+            $wt->save();
+
+            /*Mail::to($pps->Student->User->email)->send(
+                new ApproveWeeklyTrackingEmail(
+                    $pps->Student->name,
+                    $pps->id,
+                    $pps->Teacher->User->email
+                )
+            );*/
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Seguimiento aceptado correctamente'
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'title' => 'Error al aceptar el seguimiento',
+                'message' => 'Intente nuevamente o comuníquese para soporte',
+                'error' => $e->getMessage()
+            ], 400);
+        }
+    }
+
+    public function download($id)
+    {
+        try {
+            $wt = WeeklyTracking::find($id)->load('PPS');
+            $user = User::where('user_id', auth()->user()->id)->first();
+            if ($wt->PPS->student_id != $user->id && auth()->user()->role_id == 2) {
+                return response()->json([
+                    'success' => false,
+                    'title' => 'Error al descargar el seguimiento semanal',
+                    'message' => 'No está autorizado a realizar esta descarga'
+                ], 400);
+            } else if ($wt->PPS->teacher_id != $user->id && auth()->user()->role_id == 3) {
+                return response()->json([
+                    'success' => false,
+                    'title' => 'Error al descargar el seguimiento semanal',
+                    'message' => 'No está autorizado a realizar esta descarga'
+                ], 400);
+            }
+
+            if (Storage::exists($wt->file_path)) {
+                return response()->download(storage_path('app/' . $wt->file_path));
+            }
+            return response()->json([
+                'success' => false,
+                'title' => 'Error al descargar el seguimiento semanal',
+                'message' => 'El archivo no existe'
+            ], 400);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'title' => 'Error al descargar el seguimiento semanal',
+                'message' => 'Intente nuevamente o comuníquese para soporte',
+                'error' => $e->getMessage()
+            ], 400);
+        }
+    }
+}
