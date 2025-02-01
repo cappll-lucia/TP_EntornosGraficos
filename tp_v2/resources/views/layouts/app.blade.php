c671c91c6d49c64693ed8ef3d49c0550f86b0c27
rocifani <rociobfani@hotmail.com>
2025-02-01 20:37:52 -0300

    fixes deifnitivos estilos

--- tp_v2/resources/views/layouts/app.blade.php
+++ tp_v2/resources/views/layouts/app.blade.php
@@ -8,8 +8,8 @@
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('fontawesome-free-6.5.1-web/css/all.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="icon" type="image/png" href="{{ asset('favicon-utn.png') }}">
 
     <title></title>
 </head>
@@ -26,9 +26,7 @@
     @include('layouts.navigation')
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+
 </body>
 
 </html>
@@ -92,6 +90,5 @@ html, body {
     flex-grow: 1;
 }
 
-  
 
 </style>
