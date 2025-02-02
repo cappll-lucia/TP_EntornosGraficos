fd32d7bc88c7dae961547260f8b9fdb5f50b2dbe
AgustinaRodriguez01 <agus4956@gmail.com>
2025-02-01 21:48:12 -0300

    update Vite configuration to enable HTTPS in development and generate a manifest for Laravel

--- tp_v2/vite.config.js
+++ tp_v2/vite.config.js
@@ -8,8 +8,14 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        outDir: "public/build",
-        assetsDir: 'assets',
-    },
+    // build: {
+    //     outDir: "public/build",
+    //     assetsDir: 'assets',
+    // },
+    server: {
+        https: true, // Esto asegura que Vite sirva los activos a través de HTTPS en desarrollo
+      },
+      build: {
+        manifest: true, // Esto es importante para que Vite genere un manifiesto de los archivos para Laravel
+      },
 });
