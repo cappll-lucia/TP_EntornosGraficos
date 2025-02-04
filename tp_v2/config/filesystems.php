5bc4c18546a7a459d0bf504a44f674d4f5e18ec4
AgustinaRodriguez01 <agus4956@gmail.com>
2025-02-04 17:32:15 -0300

    cambio en filesystem

--- tp_v2/config/filesystems.php
+++ tp_v2/config/filesystems.php
@@ -30,19 +30,34 @@ return [
 
     'disks' => [
 
+        // 'local' => [
+        //     'driver' => 'local',
+        //     'root' => storage_path('app'),
+        //     'throw' => false,
+        // ],
+
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => '/app/storage',
             'throw' => false,
         ],
 
+        // 'public' => [
+        //     'driver' => 'local',
+        //     'root' => storage_path('app/public'),
+        //     'url' => env('APP_URL').'/storage',
+        //     'visibility' => 'public',
+        //     'throw' => false,
+        // ],
+
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => '/app/storage/public',
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
+
 
         's3' => [
             'driver' => 's3',
